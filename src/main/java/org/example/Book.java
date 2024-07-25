09307d687fec2565610e665637e4021807361dc5
GreggtheDev <greggthedev@gmail.com>
2024-07-25 16:53:42 -0400

    created Book class

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.ArrayList; // Importing ArrayList to store Book objects
+import java.util.Scanner; // Importing Scanner to read user input
+
+class Book {
+    private String title; // Attribute to store the title of the book
+    private int numberOfPages; // Attribute to store the number of pages
+    private int publicationYear; // Attribute to store the publication year

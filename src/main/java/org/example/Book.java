8342cd6e8d02f030b9d34352c86916fb86e53e63
GreggtheDev <greggthedev@gmail.com>
2024-07-25 17:00:42 -0400

    added constructor to initialize Book object

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -7,3 +7,10 @@ class Book {
     private String title; // Attribute to store the title of the book
     private int numberOfPages; // Attribute to store the number of pages
     private int publicationYear; // Attribute to store the publication year
+
+    // Constructor to initialize Book object with title, numberOfPages, and publicationYear attributes
+    public Book(String title, int numberOfPages, int publicationYear) {
+        this.title = title;
+        this.numberOfPages = numberOfPages;
+        this.publicationYear = publicationYear;
+    }

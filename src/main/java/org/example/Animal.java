5e4d57f14ce2d976055ff88809eb5092fe77c597
GreggtheDev <greggthedev@gmail.com>
2024-07-25 15:16:57 -0400

    added attributes to store the name of the animal and  if the animal is a og or not

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -3,4 +3,6 @@ package org.example;
 import java.util.ArrayList; // Importing ArrayList to store Animal objects
 import java.util.Scanner; // Importing Scanner to read user input
 
-
+class Animal {
+    private String name; // Attribute to store the name of the animal
+    private boolean isDog; // Attribute to store if the animal is a dog or not

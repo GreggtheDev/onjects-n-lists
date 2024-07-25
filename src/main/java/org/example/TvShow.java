e693be6aa079f2e46a120d405bb08957d5648b23
GreggtheDev <greggthedev@gmail.com>
2024-07-25 16:17:29 -0400

    added attributes to store name, genre and number of episodes

--- src/main/java/org/example/TvShow.java
+++ src/main/java/org/example/TvShow.java
@@ -2,3 +2,8 @@ package org.example;
 
 import java.util.ArrayList; // Importing ArrayList to store TvShow objects
 import java.util.Scanner; // Importing Scanner to read user input
+
+class TvShow {
+    private String showName; // Attribute to store the name of the TV show
+    private int numberOfEpisodes; // Attribute to store the number of episodes
+    private String genre; // Attribute to store the genre of the TV show
